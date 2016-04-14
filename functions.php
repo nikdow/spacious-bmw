9bf8fdfe658f021fd61e6ceaabbdec03106fe453
cbdweb <nik@cbdweb.net>
2016-04-14 20:47:45 +1000

    remove author from post-meta; fix widget archives to show private posts; make <!-- more --> instead of excerpts;

--- functions.php
+++ functions.php
@@ -11,3 +11,45 @@ function customarchives_where( $x ) {
 if ( is_user_logged_in() ) {
     add_filter( 'getarchives_where', 'customarchives_where' );
 }
+
+/**
+ * Shows meta information of post.
+ */
+function spacious_entry_meta() {
+   if ( 'post' == get_post_type() ) :
+      echo '<footer class="entry-meta-bar clearfix">';
+      echo '<div class="entry-meta clearfix">';
+      
+      $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+      if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+         $time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
+      }
+      $time_string = sprintf( $time_string,
+         esc_attr( get_the_date( 'c' ) ),
+         esc_html( get_the_date() ),
+         esc_attr( get_the_modified_date( 'c' ) ),
+         esc_html( get_the_modified_date() )
+      );
+      printf( __( '<span class="date"><a href="%1$s" title="%2$s" rel="bookmark">%3$s</a></span>', 'spacious' ),
+         esc_url( get_permalink() ),
+         esc_attr( get_the_time() ),
+         $time_string
+      ); ?>
+
+      <?php if( has_category() ) { ?>
+         <span class="category"><?php the_category(', '); ?></span>
+      <?php } ?>
+
+      <?php if ( comments_open() ) { ?>
+         <span class="comments"><?php comments_popup_link( __( 'No Comments', 'spacious' ), __( '1 Comment', 'spacious' ), __( '% Comments', 'spacious' ), '', __( 'Comments Off', 'spacious' ) ); ?></span>
+      <?php } ?>
+
+      <?php if ( ( spacious_options( 'spacious_archive_display_type', 'blog_large' ) != 'blog_full_content' ) && !is_single() ) { ?>
+         <span class="read-more-link"><a class="read-more" href="<?php the_permalink(); ?>"><?php _e( 'Read more', 'spacious' ); ?></a></span>
+      <?php } ?>
+
+      <?php
+      echo '</div>';
+      echo '</footer>';
+   endif;
+}
