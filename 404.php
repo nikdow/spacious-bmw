d939fe028c7929559c8da2e9ea9aea5f30a09816
nikdow <nik@cbdweb.net>
2023-01-02 18:56:57 +1100

    404 template handles private pages;

--- 404.php
+++ 404.php
@@ -10,28 +10,35 @@
 
 <?php get_header(); ?>
 
-	<?php do_action( 'spacious_before_body_content' ); ?>
+	<?php do_action( 'spacious_before_body_content' );
 
-	<div id="primary">
-		<div id="content" class="clearfix">
-			<section class="error-404 not-found">
-				<div class="page-content">
+		$page_requested = get_page_by_path($_SERVER['REQUEST_URI']);
+		$page_status = get_post_status( $page_requested->ID );
+		if( $page_status == 'private' && ! is_user_logged_in() ){
+			echo "<h2>This page requires a member login, please login below to view this page</h2>";
+		} else { ?>
 
-					<?php if ( ! dynamic_sidebar( 'spacious_error_404_page_sidebar' ) ) : ?>
-						<header class="page-header">
-							<h2 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'spacious' ); ?></h2>
-						</header>
-						<p><?php _e( 'It looks like nothing was found at this location. Try the search below, or <br/>
-						<b>login below to view member pages</b>.', 'spacious' ); ?></p>
-						<?php get_search_form(); ?>
-					<?php endif; ?>
+            <div id="primary">
+                <div id="content" class="clearfix">
+                    <section class="error-404 not-found">
+                        <div class="page-content">
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-		</div><!-- #content -->
-	</div><!-- #primary -->
+                            <?php if ( ! dynamic_sidebar( 'spacious_error_404_page_sidebar' ) ) : ?>
+                                <header class="page-header">
+                                    <h2 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'spacious' ); ?></h2>
+                                </header>
+                                <p><?php _e( 'It looks like nothing was found at this location. Try the search below, or <br/>
+                                <b>login below to view member pages</b>.', 'spacious' ); ?></p>
+                                <?php get_search_form(); ?>
+                            <?php endif; ?>
 
-	<?php spacious_sidebar_select(); ?>
+                        </div><!-- .page-content -->
+                    </section><!-- .error-404 -->
+                </div><!-- #content -->
+            </div><!-- #primary -->
+        <?php }
+
+	spacious_sidebar_select(); ?>
 
 	<?php do_action( 'spacious_after_body_content' ); ?>
 
